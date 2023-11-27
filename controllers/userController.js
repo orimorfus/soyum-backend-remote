dcb9feee4d4f87f4b66a7fe62e7dea5b431b6afd
feiron <pawel.rogowski.praca@gmail.com>
2023-11-27 04:20:27 +0100

    feat: delete account endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,6 +73,14 @@ const deleteAccount = async (req, reply) => {
   reply.send('User deleted successfully');
 };
 
+const logout = async (req, reply) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
+  const blacklistEntry = new Blacklist({ token, expiresAt });
+  await blacklistEntry.save();
+  reply.send('Logged out successfully');
+};
+
 module.exports = {
   register,
   login,
