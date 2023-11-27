1acf17d1ef4a0524d0d2efb993734b4cc748b4c9
feiron <pawel.rogowski.praca@gmail.com>
2023-11-27 12:16:53 +0100

    feat: implemented swagger docs and logger

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,179 @@ const { register, login, logout, deleteAccount } = require('../controllers/userC
 const verifyJWT = require('../middleware/authMiddleware');
 
 module.exports = (fastify, opts, done) => {
-  fastify.post('/register', register);
-  fastify.post('/login', login);
+  fastify.post(
+    '/register',
+    {
+      schema: {
+        description: 'Register a new user',
+        tags: ['User'],
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            email: { type: 'string' },
+            password: { type: 'string' },
+          },
+          required: ['name', 'email', 'password'],
+        },
+        response: {
+          201: {
+            description: 'User registered successfully',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+              user: { type: 'object' },
+              token: { type: 'string' },
+            },
+          },
+          400: {
+            description: 'Bad Request',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+          500: {
+            description: 'Internal Server Error',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    register
+  );
 
-  fastify.get('/logout', { preValidation: [verifyJWT] }, logout);
-  fastify.get('/deleteaccount', { preValidation: [verifyJWT] }, deleteAccount);
+  fastify.post(
+    '/login',
+    {
+      schema: {
+        description: 'Login a user',
+        tags: ['User'],
+        body: {
+          type: 'object',
+          properties: {
+            email: { type: 'string' },
+            password: { type: 'string' },
+          },
+          required: ['email', 'password'],
+        },
+        response: {
+          200: {
+            description: 'User logged in successfully',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+              user: { type: 'object' },
+              token: { type: 'string' },
+            },
+          },
+          400: {
+            description: 'Bad Request',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+          500: {
+            description: 'Internal Server Error',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    login
+  );
+
+  fastify.get(
+    '/logout',
+    {
+      schema: {
+        description: 'Logout a user',
+        tags: ['User'],
+        response: {
+          200: {
+            description: 'User logged out successfully',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+          400: {
+            description: 'Bad Request',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+          500: {
+            description: 'Internal Server Error',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+      preValidation: [verifyJWT],
+    },
+    logout
+  );
+
+  fastify.get(
+    '/deleteaccount',
+    {
+      schema: {
+        description: 'Delete a user account',
+        tags: ['User'],
+        response: {
+          200: {
+            description: 'User account deleted successfully',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+          400: {
+            description: 'Bad Request',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+          500: {
+            description: 'Internal Server Error',
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+      preValidation: [verifyJWT],
+    },
+    deleteAccount
+  );
   done();
 };
