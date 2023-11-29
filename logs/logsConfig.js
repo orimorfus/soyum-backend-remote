9eb595892e61db7b7f4ef215d1a8ba977230c2d7
feiron <pawel.rogowski.praca@gmail.com>
2023-11-29 01:01:29 +0100

    updated logger

--- logs/logsConfig.js
+++ logs/logsConfig.js
@@ -12,22 +12,6 @@ const logger = {
     },
   },
   redact: ['password', 'token', 'refreshToken'],
-  stream: require('pino').multistream([
-    {
-      stream: require('pino-pretty')({
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-        messageFormat: '{msg} {req.method} {req.url}]',
-      }),
-    },
-    {
-      stream: require('rotating-file-stream').createStream('logs/logs.log', {
-        size: '10M',
-        compress: 'gzip',
-        interval: '1d',
-      }),
-    },
-  ]),
 };
 
 module.exports = logger;
