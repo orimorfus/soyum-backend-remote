7bd1999670a829a871e5342d9143a1cf63895462
feiron <pawel.rogowski.praca@gmail.com>
2023-12-10 15:59:53 +0100

    updated mutex not releasing

--- controllers/user/loginController.js
+++ controllers/user/loginController.js
@@ -10,60 +10,62 @@ const mutex = new Mutex();
 const loginController = async (req, reply) => {
   const release = await mutex.acquire();
 
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
-  if (!user) {
-    return reply.status(400).send({ message: 'Invalid credentials' });
-  }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return reply.status(400).send({ message: 'Invalid credentials' });
+    }
 
-  const isPasswordValid = await comparePasswords(password, user.password);
-  if (!isPasswordValid) {
-    return reply.status(400).send('Invalid credentials');
-  }
+    const isPasswordValid = await comparePasswords(password, user.password);
+    if (!isPasswordValid) {
+      return reply.status(400).send('Invalid credentials');
+    }
 
-  const oldRefreshTokens = await RefreshToken.find({
-    userId: user._id,
-    device: req.deviceId,
-  });
-  for (const oldRefreshToken of oldRefreshTokens) {
-    await blacklistRefreshToken(
-      oldRefreshToken.token,
+    const oldRefreshTokens = await RefreshToken.find({
+      userId: user._id,
+      device: req.deviceId,
+    });
+    for (const oldRefreshToken of oldRefreshTokens) {
+      await blacklistRefreshToken(
+        oldRefreshToken.token,
+        user._id,
+        req.deviceId,
+        oldRefreshToken.expiresAt
+      );
+    }
+
+    const accessToken = generateAccessToken(user._id, process.env.ACCESS_TOKEN_EXPIRATION);
+    const refreshToken = generateRefreshToken(
       user._id,
       req.deviceId,
-      oldRefreshToken.expiresAt
+      process.env.REFRESH_TOKEN_EXPIRATION
     );
+
+    const refreshTokenDoc = new RefreshToken({
+      token: refreshToken,
+      userId: user._id,
+      device: req.deviceId,
+    });
+    await refreshTokenDoc.save();
+
+    reply.code(200).send({
+      message: 'Logged in successfully',
+      user: {
+        name: user.name,
+        email: user.email,
+        isEmailConfirmed: user.isEmailConfirmed,
+        avatarUrl: user.avatarUrl,
+      },
+      tokens: {
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      },
+    });
+  } finally {
+    release();
   }
-
-  const accessToken = generateAccessToken(user._id, process.env.ACCESS_TOKEN_EXPIRATION);
-  const refreshToken = generateRefreshToken(
-    user._id,
-    req.deviceId,
-    process.env.REFRESH_TOKEN_EXPIRATION
-  );
-
-  const refreshTokenDoc = new RefreshToken({
-    token: refreshToken,
-    userId: user._id,
-    device: req.deviceId,
-  });
-  await refreshTokenDoc.save();
-
-  reply.code(200).send({
-    message: 'Logged in successfully',
-    user: {
-      name: user.name,
-      email: user.email,
-      isEmailConfirmed: user.isEmailConfirmed,
-      avatarUrl: user.avatarUrl,
-    },
-    tokens: {
-      accessToken: accessToken,
-      refreshToken: refreshToken,
-    },
-  });
-
-  release();
 };
 
 module.exports = loginController;
